46dcb7b5ea5e72c2bcc1c9d8fe79cf1d0479bf3c
ProgrammerFront077 <alirezakh0090@gmail.com>
2023-06-15 13:27:53 +0430

    (Upate):<-Body class->

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,5 +1,40 @@
 const Home = () => {
-  return <>Home</>;
+  return (
+    <p className="dark:text-white text-black">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+      consequuntur, molestias suscipit vel esse tempora quibusdam quae! Corrupti
+      autem pariatur accusantium illum fugit quo ducimus deleniti aliquid.
+      Quibusdam, sint aliquam. Dolorem fuga minima deserunt. Officia, eligendi
+      nesciunt et expedita hic doloribus maiores iste sint! Corrupti hic, atque
+      expedita natus ex deserunt maiores nam perspiciatis earum quas iste eaque
+      dolorem nisi. Rerum vel a voluptate, quia libero cumque consequuntur
+      inventore, molestias atque sequi, tenetur praesentium ducimus quos fugiat
+      odio corrupti? Qui laborum reprehenderit nihil ullam voluptate cumque
+      nisi, esse dolore sed! Natus doloremque eius tempore repudiandae quod
+      delectus nemo laboriosam neque, aut velit non magnam voluptates expedita
+      et laudantium repellat temporibus ea aperiam odit animi rem itaque
+      asperiores fuga eveniet. Temporibus? Placeat, eum. Veniam perspiciatis in
+      nostrum animi itaque quasi doloremque exercitationem accusantium, officiis
+      expedita delectus libero repellendus sint mollitia fugit facilis
+      reiciendis explicabo doloribus molestiae! Doloremque odit voluptatum fuga
+      fugiat. Nesciunt autem facilis officiis repellendus est vitae ex omnis
+      laboriosam minus, ducimus corporis nemo, quidem ullam enim corrupti error!
+      Ex dolore nisi omnis dolor provident consectetur libero fugit iure
+      numquam! Modi excepturi eos, consequatur aliquid maxime voluptatibus autem
+      voluptatem ullam dicta magnam ad ex recusandae beatae iste velit,
+      temporibus reprehenderit. Debitis nam exercitationem sit aliquid iusto qui
+      non ut cumque! Neque asperiores, nobis consequuntur doloremque minima
+      soluta, ratione non magnam similique a itaque, pariatur impedit saepe
+      doloribus id dolores animi vitae unde atque quaerat dolorum eius fugit
+      sint possimus. Blanditiis. Magni fugiat vitae, fuga reiciendis ratione
+      iure ipsum debitis minus, repellat, distinctio iusto officia tenetur
+      molestiae est beatae odit! Adipisci dolore pariatur hic assumenda, eum
+      tempore iure praesentium corrupti. Repellat. Blanditiis tempore est modi
+      eaque aliquam aut ut fugiat unde repellendus fuga, corrupti perspiciatis,
+      consequuntur labore corporis autem neque facere consequatur exercitationem
+      eligendi omnis quo veniam. Ab aperiam mollitia omnis.
+    </p>
+  );
 };
 
 export default Home;
